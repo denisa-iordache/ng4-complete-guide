bf6db62b9cd02677649854c3dd993e2835785827
Denisa <denisa.iordache@getik.net>
2024-11-05 14:16:31 +0200

    part 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -36,6 +36,7 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     ApppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,44 +8,51 @@ export class RecipeService {
     // recipeSelected = new EventEmitter<Recipe>();
     recipedChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Chicken Schnitzel', 'A super tasty chicken schnitzel!', 'https://www.prestij.ro/uploads/produse/660x660/253--snitel-de-pui-in-fulgi-de-porumb-cu-cartofi-prajiti-150-200g.jpg',
-        [new Ingredient('Chicken Meat', 1),
-        new Ingredient('Salad', 1),
-        new Ingredient('Fries', 1)]),
-        new Recipe('Lava Cake', 'Did somebody say dessert? Try the best lava cake!', 'https://cookiesfordays.com/wp-content/uploads/2024/01/molten-lava-cakes-10.jpg',
-        [new Ingredient('Cocoa', 1),
-        new Ingredient('Vanilla Ice Cream', 1),
-        new Ingredient('Milk', 1),
-        new Ingredient('Flour', 1),
-        new Ingredient('Eggs', 3)])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Chicken Schnitzel', 'A super tasty chicken schnitzel!', 'https://www.prestij.ro/uploads/produse/660x660/253--snitel-de-pui-in-fulgi-de-porumb-cu-cartofi-prajiti-150-200g.jpg',
+    //         [new Ingredient('Chicken Meat', 1),
+    //         new Ingredient('Salad', 1),
+    //         new Ingredient('Fries', 1)]),
+    //     new Recipe('Lava Cake', 'Did somebody say dessert? Try the best lava cake!', 'https://cookiesfordays.com/wp-content/uploads/2024/01/molten-lava-cakes-10.jpg',
+    //         [new Ingredient('Cocoa', 1),
+    //         new Ingredient('Vanilla Ice Cream', 1),
+    //         new Ingredient('Milk', 1),
+    //         new Ingredient('Flour', 1),
+    //         new Ingredient('Eggs', 3)])
+    // ];
 
-    constructor(private slService: ShoppingListService){}
+    private recipes: Recipe[] = [];
 
-    getRecipes(){
+    constructor(private slService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipedChanged.next(this.recipes.slice());
+    }
+
+    getRecipes() {
         return this.recipes.slice();
     }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
     }
 
-    getRecipe(id: number){
+    getRecipe(id: number) {
         return this.recipes[id];
     }
 
-    addRecipe(recipe: Recipe){
+    addRecipe(recipe: Recipe) {
         this.recipes.push(recipe);
         this.recipedChanged.next(this.recipes.slice())
     }
 
-    updateRecipe(index: number, newRecipe: Recipe){
+    updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
         this.recipedChanged.next(this.recipes.slice())
     }
 
-    deleteRecipe(index: number){
+    deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
         this.recipedChanged.next(this.recipes.slice())
     }
